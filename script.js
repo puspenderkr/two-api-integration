6a4a391cf2df021c7b3ad80c47a39c76daeef2e3
Puspender <puspenderkr2k@gmail.com>
2021-06-30 16:12:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+import {info} from './env.js';
+const body = document.querySelector('body'); 
+const button = document.querySelector('button'); 
+const wordContainer = document.querySelector('.word-container'); 
+const word = document.querySelector('h2'); 
+const definition = document.querySelector('p'); 
+ 
+ 
+
+const randomWord = () => {
+    fetch('https://random-word-api.herokuapp.com/word?number=1')
+    .then(response => {
+        return response.json(); 
+    })
+    .then(response => {
+        word.textContent = response
+        randomDefinition(word);
+        wordContainer.classList.remove('display-none'); 
+    })
+    .catch(err => {
+        console.log(err); 
+        word.textContent = "No Word Available"
+    }); 
+}
+
+const randomDefinition = (word) => {
+    fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word.textContent}?key=${info.key}`)
+    .then(response => {
+        return response.json(); 
+    })
+    .then(response => {
+        console.log(response[0].shortdef[0]); 
+        definition.classList.remove('error'); 
+        definition.textContent = "Definition: " + response[0].shortdef[0]; 
+    })
+    .catch(err => {
+       console.log(err); 
+       definition.classList.add('error'); 
+       definition.textContent = "No Definition Available"; 
+    });
+
+}
+
+button.addEventListener('click', function(){
+    randomWord(); 
+})
+
